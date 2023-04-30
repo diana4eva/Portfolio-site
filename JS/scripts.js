e00c34c423e0f98b5657fb84e623d07c19226e9a
Diana Fridelova <Difridelova@gmail.com>
2023-04-30 15:37:53 -0400

    First javascript styling

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,8 @@
-console.log("hello, welcome to my world :)")
-
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    x.className += ' responsive';
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
 }
